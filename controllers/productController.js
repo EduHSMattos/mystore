dcb3df42b5be44f169b76a8450cdc9a9e85a6c90
EduHSMattos <eduhmattos@hotmail.com>
2023-11-08 18:10:42 -0300

    First commit

---
+++ controllers/productController.js
@@ -0,0 +1,122 @@
+const filesystem = require('fs').promises;
+const path = require('path');
+const { stringify } = require('querystring');
+
+const productFilePath = path.join(__dirname, '../data/products.json');
+
+const getProducts = () => {
+    return filesystem.readFile(productFilePath, 'utf-8')
+        .then((productsData) => JSON.parse(productsData))
+        .catch((error) => {
+            throw new Error('Não foi possível ler o arquivo de produtos!')
+        });
+};
+
+const getProductById = (productId) => {
+    return getProducts()
+        .then((allProducts) => allProducts.find(
+            product =>  product.id === parseInt(productId)
+        ))
+        .catch((error) => {
+            throw new Error('Não foi possível encontrar o produto')
+        });
+};
+
+const searchProductByName = (productName) => {
+    return getProducts()
+        .then((productsData) => {
+            const filtredProds = productsData.filter((product) => 
+                product.title.toLowerCase().includes(productName.toLowerCase())
+            );
+            
+            return filtredProds;
+        })
+        .catch((error) => {
+            throw new Error('Não foi possível encontrar produtos pelo nome')
+        })
+};
+
+const updateProduct = (productId, updatedData) => {
+    return getProducts()
+        .then((productsData) => {
+            const productIndex = productsData.findIndex(
+                product => product.id === parseInt(productId)
+            )
+
+            if(productIndex != -1){
+                const existingProduct = productsData[productIndex];
+
+                if(updatedData.title != undefined){
+                    existingProduct.title = updatedData.title;
+                } 
+                if(updatedData.price != undefined){
+                    existingProduct.price = updatedData.price;
+                }
+
+                productsData[productIndex] = existingProduct;
+
+                return filesystem.writeFile(productFilePath, JSON.stringify(productsData, null, 2), 'utf-8')
+                    .then(() => {
+                        return existingProduct;
+                    })
+                    .error((error) => {
+                        throw new Error('Não foi possível atualizar o produto!')
+                    })
+
+            } else {
+                throw new Error('Não foi encontrado produto com esse id!');
+            }
+
+            }
+        )
+        .catch((error) => {
+            throw new Error('Não possível ler os produtos!');
+        })
+}
+
+const deleteProducts = (productId) => {
+    return getProducts()
+        .then((productsData) => {
+            const productIndex = productsData.findIndex(
+                product => product.id === parseInt(productId) 
+            )
+
+            if (productIndex != -1){
+
+                const updatedProductsData = ProductsData.filter(
+                    product => product.id !== parseInt(productId)
+                )
+
+                const deletedProduct = productsData[productIndex];
+                
+                return filesystem.writeFile(productFilePath, JSON.stringify(updatedProductsData, null, 2), 'utf-8')    
+                    .then(() => {
+                        return deletedProduct;
+                    })
+                    .catch((error) => {
+                        throw new Error ('Não foi possível excluir o produto!')
+                    })
+            
+
+            } else {
+                throw new Error ('Produto não encontrado!')
+            }        
+
+
+
+
+
+        })
+
+        .catch((error) => {
+            throw new Error ('Não possível apagar os produtos!')
+        })
+}
+
+module.exports = {
+    getProducts,
+    getProductById,
+    searchProductByName,
+    updateProduct,
+    deleteProducts
+}
---
+++ routes/productRoutes.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController')
+
+router.get('/', (req, res) => {
+    productController.getProducts()
+        .then((productsData) => res.json(productsData))
+        .catch((error) => res.status(500).send('Erro ao obter produtos'))
+})
+
+router.get('/:id', (req, res) => {
+    const IdRecebido = req.params.id;
+    productController.getProductById(IdRecebido)
+        .then((productRecebido) => {
+            if(productRecebido){
+                res.status(200).send(productRecebido);
+            } else {
+                res.status(404).send('Produto não encontrado');
+            }
+        })
+        .catch((error) => res.status(500).send())
+})
+
+router.get('/search/:name', (req, res) => {
+    const productName = req.params.name;
+    productController.searchProductByName(productName)
+        .then((products) => {
+            if(products){
+                res.status(200).send(products)
+            } else {
+                res.status(404).send('John Travoltas time!!!!')
+            }
+        })
+        .catch((error) => res.status(500).send())
+})
+
+router.put('/:id', (req, res) => {
+    const idRecebido = req.params.id;
+    const updatedData = req.body;
+
+    console.log("Id recebido: " + idRecebido)
+    console.log("Informações recebidas: " + updatedData.title)
+
+    productController.updateProduct(idRecebido, updatedData)
+        .then(() =>{
+            res.status(200).send("Produto atualizado com sucesso!")
+        })
+        .catch((error) => {
+           res.send(error);
+        })
+})
+
+router.delete('/id', (req, res) => {
+    const idRecebido = req.params.id;
+
+    productController.deleteProducts(idRecebido)
+        .then((deletedProducts) => {   
+                res.status(200).json(deletedProducts);
+        })
+        .catch((error) => {
+            res.status(404).send('Produto não encontrado');
+        })
+})
+
+
+module.exports = router;
